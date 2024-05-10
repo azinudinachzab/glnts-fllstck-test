97a72658eb87300d2dcb1ba72012febfa35698b7
Azinudin Achzab <azinudinachzab@users.noreply.github.com>
2024-05-10 10:38:44 +0700

    Update solution-1-2.sql

--- question-1/solution-1-2.sql
+++ question-1/solution-1-2.sql
@@ -28,5 +28,18 @@ SELECT
         END AS tally_status
 FROM
     po_totals po
-        JOIN
-    cer_totals cer ON po.pr_id = cer.pr_id;
+        LEFT JOIN
+    cer_totals cer ON po.pr_id = cer.pr_id
+UNION
+SELECT
+    po.pr_id,
+    po.total_po_items AS total_po_items,
+    cer.total_cer_items AS total_cer_items,
+    CASE
+        WHEN po.total_po_items = cer.total_cer_items THEN 'Tally'
+        ELSE 'Not Tally'
+        END AS tally_status
+FROM
+    po_totals po
+        RIGHT JOIN
+    cer_totals cer ON po.pr_id = cer.pr_id
